b66faf0f0be29e56f0b3065f00e9f19fa254a5ba
Renato de Souza <renbstux@gmail.com>
2020-12-06 17:25:28 -0300

    Primeiro Commit

---
+++ buscar-cursos.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$httpClient = new \GuzzleHttp\Client([
+    'base_uri' => 'https://www.alura.com.br/',
+    'verify' => false
+]);
+
+$crawler = new Crawler();
+
+
+$buscador = new Alura\BuscadorDeCursos\Buscador($httpClient, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo exibeMensagem($curso);
+}
